e4d5d6550058c6de3f215db29395703336d9c93d
srikarkodakandla <nagaraj544321@gmail.com>
2024-11-11 13:49:44 +0530

    completed program1 and program2

--- program1.js
+++ program1.js
@@ -1,8 +1,32 @@
 const getTotalIsles = function (grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
 
+  const dfs = (r, c) => {
+    if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === 'W') {
+      return;
+    }
 
-  // write your code here
+    grid[r][c] = 'W';
 
+    dfs(r - 1, c);
+    dfs(r + 1, c);
+    dfs(r, c - 1);
+    dfs(r, c + 1);
+  };
+
+  let count = 0;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 'L') {
+        dfs(i, j);
+        count++;
+      }
+    }
+  }
+
+  return count;
 };
 
 module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,28 @@
 const decodeTheRing = function (s, p) {
+  const m = s.length;
+  const n = p.length;
 
-    // write your code here
+  const dp = Array(m + 1).fill(null).map(() => Array(n + 1).fill(false));
 
-  };
-  
-  module.exports = decodeTheRing;
+  dp[0][0] = true;
+
+  for (let j = 1; j <= n; j++) {
+    if (p[j - 1] === '*') {
+      dp[0][j] = dp[0][j - 1];
+    }
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (p[j - 1] === s[i - 1] || p[j - 1] === '?') {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else if (p[j - 1] === '*') {
+        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+      }
+    }
+  }
+
+  return dp[m][n];
+};
+
+module.exports = decodeTheRing;
